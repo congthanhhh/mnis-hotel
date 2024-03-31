71f8fb101dd8621e7d24fa20465a31b50b860c45
congthanhhh <luffymonkeydct@gmail.com>
2024-03-31 21:38:09 +0700

    first commit

---
+++ src/main/java/com/thanh/mnishotel/MnisHotelApplication.java
@@ -0,0 +1,13 @@
+package com.thanh.mnishotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnisHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnisHotelApplication.class, args);
+	}
+
+}
